f45896fb2cfecd2289ce06f3959ad608d4451290
mcnwr76 <anwaraan998@gmail.com>
2024-08-19 10:28:11 +0700

    update layout

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,4 +1,5 @@
-import ListMenu from "@/components/macro/Menu";
+import ListMenu from "@/components/feature/Menu";
+import Link from "next/link";
 
 interface Props {
   children: Readonly<React.ReactNode>;
@@ -7,15 +8,17 @@ interface Props {
 const Header = ({ children }: Props) => {
   return (
     <div className="flex">
-      <div className="flex flex-col w-1/4 ">
-        <span className="text-4xl mx-auto font-bold font-serif ">BRAND</span>
+      <div className="flex flex-col w-1/4">
+        <Link href={"/home"} className="text-4xl mx-auto font-bold font-serif ">
+          BRAND
+        </Link>
         <div className="mx-auto text-base lg:text-xl my-auto">
           <ListMenu />
         </div>
       </div>
 
-      <div className="flex w-1/2 px-8 border-r border-l border-sky-500 overflow-scroll h-screen">{children}</div>
-      <div className="flex w-1/4 mx-auto">
+      <div className="flex w-2/4 border-r border-l border-sky-500 overflow-scroll h-screen">{children}</div>
+      <div className="flex w-1/4">
         <h1>Suggestion</h1>
       </div>
     </div>
