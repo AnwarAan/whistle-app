9feaadd99edcc62edc4cfb1a0944012e7afbf393
mcnwr76 <anwaraan998@gmail.com>
2024-08-19 10:27:23 +0700

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,10 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get("token")?.value;
-  if (!token) return NextResponse.redirect(new URL("/auth", request.url));
-
-  const res = NextResponse.next();
-  return res.cookies.get("token");
+export async function middleware(request: NextRequest, res: NextResponse) {
+  const token = request.cookies.get("whistle_token")?.value;
+  if (!token) return NextResponse.redirect(new URL("/home", request.url));
+  NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
